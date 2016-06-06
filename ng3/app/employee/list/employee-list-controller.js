b593ed541c913888a9f30f3e351e0c3dca33e64d
Sergii Nesprava <serg.seven@gmail.com>
2016-06-07 00:10:11 +0200

    next sync

---
+++ ng3/app/employee/list/employee-list-controller.js
@@ -0,0 +1,2 @@
+angular.module('myApp.view1').controller('EmployeeListCtrl', [function($scope) {
+}]);
--- ng3/app/employee/list/employee-list.js
+++ ng3/app/employee/list/employee-list.js
@@ -9,6 +9,3 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
